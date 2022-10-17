06c329730e38eb423e90b18d342d09fc12f1c32d
Logan <monday.hopscotch@gmail.com>
2022-10-17 00:23:41 -0600

    significant upgrade to the shader code

--- source/LightingShader.hx
+++ source/LightingShader.hx
@@ -7,25 +7,28 @@ class LightingShader extends FlxShader {
 	@:glFragmentSource('
 		#pragma header
 		uniform sampler2D normalTex;
+
+		uniform float numLights;
+
 		uniform vec2 lightPos1;
 		uniform vec2 lightPos2;
 		uniform vec2 lightPos3;
-		uniform vec2 lightPos4;
-		uniform vec2 lightPos5;
-		uniform vec2 lightPos6;
-		uniform vec2 lightPos7;
-		uniform vec2 lightPos8;
-		uniform vec2 lightPos9;
-		uniform vec2 lightPos0;
-		uniform float numLights;
-		uniform float lightHeight;
-		uniform float ambientRatio;
+		uniform vec3 lightColor1;
+		uniform vec3 lightColor2;
+		uniform vec3 lightColor3;
+		uniform float lightHeight1;
+		uniform float lightHeight2;
+		uniform float lightHeight3;
+
+		uniform vec3 ambientColor;
+		uniform float ambientStrength;
+
 		uniform float aspectRatio;
 
-		vec4 getLight(vec2 lPos) {
+		vec4 getLight(vec2 lPos, float lightHeight, vec3 color) {
 			// pull out the color vectors from the source image and the normal image
 			vec4 source = flixel_texture2D(bitmap, openfl_TextureCoordv);
-			vec4 normal = texture2D(normalTex, openfl_TextureCoordv);
+			vec3 normal = texture2D(normalTex, openfl_TextureCoordv).xyz;
 
 			// Can remove this if-block to have sprites without normal data
 			// be considered as though no light is hitting them
@@ -34,20 +37,15 @@ class LightingShader extends FlxShader {
 				return source;
 			}
 
-			vec2 distance = lPos - openfl_TextureCoordv;
-			distance *= vec2(1, aspectRatio);
-
-			// if (length(distance) > 1.0) {
-			// 	return vec4(0., 0., 0., 0.);
-			// }
+			vec2 distanceVec = lPos - openfl_TextureCoordv;
+			distanceVec /= vec2(1, aspectRatio);
+			float distance = length(distanceVec);
 
             // calculate the vector going from the texture coord to the light source and add some height to this vector
-			vec4 toLight = vec4(lPos - openfl_TextureCoordv, lightHeight, 0);
+			vec3 toLight = normalize(vec3(distanceVec, lightHeight));
 
 			// invert the y value since uv coords have reversed coordinate system
 			toLight.y *= -1.0;
-
-
 
 			// normalize the normal vector (honestly not sure why we need the *2.0-1 thing here, but it doesnt work without it)
 			// ANSWER:
@@ -60,24 +58,24 @@ class LightingShader extends FlxShader {
 			// calculate what the angle is between the normal vector and the pixel to the light source.
 			// An angle of 1.0 means the light source and the normal are parallel
 			// an angle of 0.0 means they are perpendicular
-			float cos_angle = dot(normal, toLight);
+			float product = max(dot(normal, toLight), 0.0);
+			vec3 diffuse = color * product;
 
-			// distance from the light
-			float dist = length(lPos - openfl_TextureCoordv);
-			float distanceImpact = clamp(dist, 0.01, 1.) / 1.;
-			distanceImpact = 1. - pow(distanceImpact, 5.);
+			// TODO: This should be contained somewhere else
+			// modifier for overall strength of specular reflections
+			float specularStrength = 0.5;
 
-			cos_angle *= distanceImpact;
-
-			// clamp the "angle" which is now basically the intensity of the light on the pixel and
-			// bring up the bottom of the clamp to match the ambientRatio which basically just makes
-			// sure each pixel is drawn with at least SOME color instead of being completely black
-			// (use 0.0 for ratio if you want completely black)
-			cos_angle = clamp(cos_angle, ambientRatio, 1.0);
+			// This assumes an orthographic viewpoint
+			vec3 camDir = vec3(0.0, 0.0, 1.0);
+			// The angle the light would reflect off of the surface
+			vec3 reflectDir = reflect(-toLight, normal);
+			// calculate the reflection against where the camera is
+			float spec = pow(max(dot(camDir, reflectDir.xyz), 0.0), 32.);
+			// final weight of specular reflection taking light color into account
+			vec3 specular = specularStrength * spec * color;
 
 			// multiply the rgb of the source color vector by the angle/intensity to either brighten or dim the color at this pixel, and just use the original alpha from the source directly, dont manipulate that with light
-			source.rgb *= cos_angle;
-			// source.a = 1.;
+			source.rgb *= (diffuse + specular);
 
 			return source;
 		}
@@ -86,60 +84,47 @@ class LightingShader extends FlxShader {
 		{
 			vec4 litColor = vec4(0.,0.,0.,0.);
 			if (numLights > 0.0) {
-				litColor = max(litColor, getLight(lightPos0));
+				litColor = max(litColor, getLight(lightPos1, lightHeight1, lightColor1));
 			}
 			if (numLights > 1.0) {
-				litColor = max(litColor, getLight(lightPos1));
+				litColor = max(litColor, getLight(lightPos2, lightHeight2, lightColor2));
 			}
 			if (numLights > 2.0) {
-				litColor = max(litColor, getLight(lightPos2));
-			}
-			if (numLights > 3.0) {
-				litColor = max(litColor, getLight(lightPos3));
-			}
-			if (numLights > 4.0) {
-				litColor = max(litColor, getLight(lightPos4));
-			}
-			if (numLights > 5.0) {
-				litColor = max(litColor, getLight(lightPos5));
-			}
-			if (numLights > 6.0) {
-				litColor = max(litColor, getLight(lightPos6));
-			}
-			if (numLights > 7.0) {
-				litColor = max(litColor, getLight(lightPos7));
-			}
-			if (numLights > 8.0) {
-				litColor = max(litColor, getLight(lightPos8));
-			}
-			if (numLights > 9.0) {
-				litColor = max(litColor, getLight(lightPos9));
+				litColor = max(litColor, getLight(lightPos3, lightHeight3, lightColor3));
 			}
 
 			vec4 source = flixel_texture2D(bitmap, openfl_TextureCoordv);
 
-			if (litColor.a < 1.) {
-				// counting lights on the alpha channel. 0 meansno lights lit this pixel;
-				litColor.rgb = source.rgb * ambientRatio;
+			vec3 ambient = (ambientColor * ambientStrength) * source.rgb;
+
+			vec3 resultColor = (ambient + litColor.rgb) * source.rgb;
+
+			gl_FragColor = vec4(resultColor, source.a);
+
+			// DEBUG for knowing where our light is
+			float maxLightSize = 0.03;
+			vec2 distance = lightPos1 - openfl_TextureCoordv;
+			distance /= vec2(1, aspectRatio);
+			if (length(distance) < .01 + maxLightSize * lightHeight1) {
+				gl_FragColor = vec4(lightColor1, 1.);
 			}
 
-			gl_FragColor = vec4(litColor.rgb, source.a);
-
-			vec2 distance = lightPos0 - openfl_TextureCoordv;
-			// distance *= vec2(1, aspectRatio);
-			if (length(distance) < .05) {
-				gl_FragColor = vec4(1., 1., 1., 1.);
+			distance = lightPos2 - openfl_TextureCoordv;
+			distance /= vec2(1, aspectRatio);
+			if (length(distance) < .01 + maxLightSize * lightHeight2) {
+				gl_FragColor = vec4(lightColor2, 1.);
 			}
 
-			// vec4 tester = flixel_texture2D(normalTex, openfl_TextureCoordv);
-			// gl_FragColor = tester;
+			distance = lightPos3 - openfl_TextureCoordv;
+			distance /= vec2(1, aspectRatio);
+			if (length(distance) < .01 + maxLightSize * lightHeight3) {
+				gl_FragColor = vec4(lightColor3, 1.);
+			}
+			// END DEBUG
 		}')
 	public function new(pixels:BitmapData) {
 		super();
 		setNormalMapPixels(pixels);
-		setLightPositions([new FlxPoint(0, 0)]);
-		setLightHeight(0.5);
-		setAmbientRatio(0.2);
 
 		var ratio = 1.0 * FlxG.width / FlxG.height;
 		setAspectRatio(ratio);
@@ -150,55 +135,32 @@ class LightingShader extends FlxShader {
 		normalTex.input = pixels;
 	}
 
-	// for this to work properly, you will need to convert your light position into local coordinates to the sprite, then into a ratio between 0-1 based on the sprite's total width (if it is a sprite sheet, you will need to take frameWidth * numOfFrames)
+	// for this to work properly, you will need to convert your light position into local coordinates for the camera in terms from 0.0-1.0
 	public function setLightPositions(positions:Array<FlxPoint>) {
 		if (positions.length > 0) {
-			lightPos0.value = [positions[0].x, positions[0].y];
+			lightPos1.value = [positions[0].x, positions[0].y];
 		}
 		if (positions.length > 1) {
-			lightPos1.value = [positions[1].x, positions[1].y];
+			lightPos2.value = [positions[1].x, positions[1].y];
 		}
 		if (positions.length > 2) {
-			lightPos2.value = [positions[2].x, positions[2].y];
-		}
-		if (positions.length > 3) {
-			lightPos3.value = [positions[3].x, positions[3].y];
-		}
-		if (positions.length > 4) {
-			lightPos4.value = [positions[4].x, positions[4].y];
-		}
-		if (positions.length > 5) {
-			lightPos5.value = [positions[5].x, positions[5].y];
-		}
-		if (positions.length > 6) {
-			lightPos6.value = [positions[6].x, positions[6].y];
-		}
-		if (positions.length > 7) {
-			lightPos7.value = [positions[7].x, positions[7].y];
-		}
-		if (positions.length > 8) {
-			lightPos8.value = [positions[8].x, positions[8].y];
-		}
-		if (positions.length > 9) {
-			lightPos9.value = [positions[9].x, positions[9].y];
+			lightPos3.value = [positions[2].x, positions[2].y];
 		}
 
-		numLights.value = [Math.min(positions.length, 10)];
+		numLights.value = [Math.min(positions.length, 3)];
 	}
 
-	// I'm not totally sure this is really what I'm saying it is, but theoretically this changes how high the light is in the "sky" compared to the flat sprite is on the "ground"
-	public function setLightHeight(height:Float) {
-		lightHeight.value = [height];
-	}
-
-	// setting this ratio to 1 will basically ignore the lightsource completely and just bathe everything in full light, while setting to 0 will cause the sprite to draw black if there is no light directly on it (black, not transparent)
-	public function setAmbientRatio(ratio:Float) {
-		if (ratio < 0)
-			ambientRatio.value = [0.0];
-		else if (ratio > 1.0)
-			ambientRatio.value = [1.0];
-		else
-			ambientRatio.value = [ratio];
+	// how much z the lights have
+	public function setLightHeights(heights:Array<Float>) {
+		if (heights.length > 0) {
+			lightHeight1.value = [heights[0]];
+		}
+		if (heights.length > 1) {
+			lightHeight2.value = [heights[1]];
+		}
+		if (heights.length > 2) {
+			lightHeight3.value = [heights[2]];
+		}
 	}
 
 	public function setAspectRatio(ratio:Float) {
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -15,7 +15,71 @@ import openfl.geom.Point;
 import openfl.geom.Rectangle;
 
 class PlayState extends LightingState {
-	public var lightPoint = FlxPoint.get(0.5, 0.5);
+	public var lightPoint = FlxPoint.get(0.4, 0.2);
+	public var lightPoint2 = FlxPoint.get(0.2, 0.8);
+	public var lightPoint3 = FlxPoint.get(0.8, 0.7);
+
+	public var lightHeight = 0.0;
+	public var lightHeight2 = 1.0;
+	public var lightHeight3 = 1.0;
+
+	override public function create():Void {
+		super.create();
+
+		for (i in 0...10) {
+			makeDiamond();
+		}
+
+		for (i in 0...10) {
+			makeCircle();
+		}
+
+		// for (i in 0...100) {
+		// 	makeUnshadedSprite();
+		// }
+
+		FlxTween.tween(this, {"lightHeight": 1.0}, 2, {type: PINGPONG});
+		FlxTween.tween(this, {"lightHeight2": 0.0}, 3, {type: PINGPONG});
+		FlxG.watch.add(this, "lightHeight", "Light1 Height:");
+		FlxG.watch.add(this, "lightHeight2", "Light2 Height:");
+		FlxG.watch.add(this, "lightHeight3", "Light3 Height:");
+
+		lightShader.lightColor1.value = [0.0, 1.0, 1.0];
+		lightShader.lightColor2.value = [1.0, 1.0, 0.0];
+		lightShader.lightColor3.value = [1.0, 0.0, 0.0];
+
+		lightShader.ambientColor.value = [1.0, 0.0, 1.0];
+		lightShader.ambientStrength.value = [0.2];
+	}
+
+	override public function update(elapsed:Float):Void {
+		super.update(elapsed);
+
+		lightShader.setLightPositions([lightPoint, lightPoint2, lightPoint3]);
+		lightShader.setLightHeights([lightHeight, lightHeight2, lightHeight3]);
+
+		if (FlxG.keys.justPressed.SPACE) {
+			toggleLightingDebugCamera();
+		}
+
+		if (FlxG.mouse.pressed) {
+			lightPoint.set(FlxG.mouse.getScreenPosition().x, FlxG.mouse.getScreenPosition().y);
+			lightPoint.x /= FlxG.width;
+			lightPoint.y /= FlxG.height;
+		}
+
+		if (FlxG.mouse.pressedRight) {
+			lightPoint2.set(FlxG.mouse.getScreenPosition().x, FlxG.mouse.getScreenPosition().y);
+			lightPoint2.x /= FlxG.width;
+			lightPoint2.y /= FlxG.height;
+		}
+
+		if (FlxG.mouse.pressedMiddle) {
+			lightPoint3.set(FlxG.mouse.getScreenPosition().x, FlxG.mouse.getScreenPosition().y);
+			lightPoint3.x /= FlxG.width;
+			lightPoint3.y /= FlxG.height;
+		}
+	}
 
 	function getRandomPath(length:Int):Array<FlxPoint> {
 		var path = [
@@ -55,34 +119,9 @@ class PlayState extends LightingState {
 
 		var sprite = new FlxSprite();
 		sprite.pixelPerfectRender = true;
+		sprite.setPosition(path[0].x, path[0].y);
 		sprite.makeGraphic(32, 32, FlxColor.GRAY);
 		add(sprite);
 		FlxTween.linearPath(sprite, path, 30, {type: PINGPONG});
-	}
-
-	override public function create():Void {
-		super.create();
-
-		for (i in 0...10) {
-			makeDiamond();
-		}
-
-		for (i in 0...10) {
-			makeCircle();
-		}
-
-		for (i in 0...10) {
-			makeUnshadedSprite();
-		}
-	}
-
-	override public function update(elapsed:Float):Void {
-		super.update(elapsed);
-
-		lightShader.setLightPositions([lightPoint]);
-
-		if (FlxG.keys.justPressed.SPACE) {
-			toggleLightingDebugCamera();
-		}
 	}
 }
